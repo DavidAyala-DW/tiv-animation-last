34be9dfd55386fd0675211aa77d0b9b30dcf33ed
Austen Morgan <austen@boundaryla.com>
2021-11-22 16:42:58 -0800

    Set up gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Tiv'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: 'JYJUsIglggG0nvFAFK_ZZRfrKryoByORlTgYSX3bLEs',
+        spaceId: 'ftg31lu3nuto'
+      }
+    },
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
+    },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}'
+          ]
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true
+          }
+        }
+      },
+    }
+  ]
+}
