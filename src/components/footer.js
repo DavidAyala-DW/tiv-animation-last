a1fc73fddcdeda9970a5ec3e01da7b2c6ad16c37
Austen Morgan <austen@boundaryla.com>
2021-12-06 18:06:21 -0800

    Responsive footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,10 +9,16 @@ export default function Footer (props) {
 
   return (
     <footer className="relative bg-gray-dark text-white/50">
-      <section className="py-16 mb-1 border-t border-gray-800/80">
-        <div className="container grid grid-cols-3 justify-between items-center">
+      <section className="py-4 lg:py-16 mb-1 border-t border-gray-800/80">
+        <div className="container flex flex-col lg:grid grid-cols-3 justify-between items-center gap-4 lg:gap-10">
           <Link to="/" className="justify-self-center">
-            <img src={tivLogo} width={119} height={57} alt="Tiv" />
+            <img
+              src={tivLogo}
+              width={119}
+              height={57}
+              alt="Tiv"
+              className="h-12 lg:h-auto"
+            />
           </Link>
           <Nav
             links={data.primaryNav.links}
@@ -25,13 +31,13 @@ export default function Footer (props) {
         </div>
       </section>
 
-      <section className="py-2 border-t border-gray-800/80">
-        <div className="container grid grid-cols-3 items-center">
-          <p className="text-[13px]">{data.legalText}</p>
+      <section className="pt-4 pb-7 md:py-2 border-t border-gray-800/80">
+        <div className="container flex flex-col md:grid grid-cols-3 items-center gap-4">
           <Nav
             links={data.secondaryNav.links}
-            className="justify-self-center gap-12 text-[15px]"
+            className="md:col-start-3 lg:col-start-2 lg:justify-self-center md:order-last gap-6 md:gap-12 text-[15px]"
           />
+          <p className="text-[13px]">{data.legalText}</p>
         </div>
       </section>
     </footer>
