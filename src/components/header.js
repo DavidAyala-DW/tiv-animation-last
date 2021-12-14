fead367021bddc6929629e93a78994dd34c6ed07
Austen Morgan <austen@boundaryla.com>
2021-12-14 15:12:34 -0800

    Improve menu a11y

--- src/components/header.js
+++ src/components/header.js
@@ -5,113 +5,127 @@ import { Divide as Burger } from 'hamburger-react'
 import MenuHex from '@/components/svg/menu-hex.svg'
 import Nav from '@/components/nav'
 import SocialNav from '@/components/social-nav'
+import WaitlistButton from '@/components/waitlist-button'
 
 import tivLogo from '@/images/tiv-logo.svg'
 import menuBg from '@/images/menu-bg.svg'
+import { FocusOn } from 'react-focus-on'
 
-export default function Header (props) {
+export default function Header(props) {
   const { data, currentPath } = props
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  function toggleMenu () {
+  function toggleMenu() {
     setIsMenuOpen((val) => !val)
   }
 
   return (
-    <>
-      <header className="fixed top-0 z-50 inset-x-0 pointer-events-none flex items-center justify-between py-6">
-        <div className="container flex md:block justify-center">
-          <Link to="/" className="pointer-events-auto">
-            <img src={tivLogo} alt="Tiv" className="h-11 md:h-10 lg:h-auto" />
-          </Link>
-        </div>
+    <FocusOn enabled={isMenuOpen} onEscapeKey={toggleMenu}>
+      <div role="presentation">
+        <header className="fixed top-0 z-50 inset-x-0 pointer-events-none flex items-center justify-between py-6">
+          <div className="container flex md:block justify-center">
+            <Link to="/" className="inline-flex pointer-events-auto">
+              <img src={tivLogo} alt="Tiv" className="h-11 md:h-10 lg:h-auto" />
+            </Link>
+          </div>
 
-        <button
-          type="button"
-          className="absolute top-2 md:top-4 lg:top-6 right-3 md:right-[70px] z-50 flex items-center gap-4 text-black hover:text-gray-900 pointer-events-auto scale-75 md:scale-100"
-          onClick={toggleMenu}
-        >
-          <span
-            className="text-[9px] font-bold uppercase text-white sr-only md:not-sr-only"
-            aria-live="polite"
+          <button
+            type="button"
+            className="absolute top-2 md:top-4 lg:top-6 right-3 md:right-[70px] z-50 flex items-center gap-4 text-black hover:text-gray-900 pointer-events-auto scale-75 md:scale-100 focus-within:outline-white"
+            onClick={toggleMenu}
           >
-            {isMenuOpen ? 'Close' : 'Menu'}
-          </span>
-          <span className="grid items-center justify-center" aria-hidden="true">
-            <MenuHex
-              className={classNames(
-                'both-span-full',
-                isMenuOpen && 'drop-shadow-xl'
-              )}
-            />
-            <span className="both-span-full flex justify-center text-orange scale-75">
-              <Burger color="currentColor" distance="sm" toggled={isMenuOpen} />
+            <span
+              className="text-[9px] font-bold uppercase text-white sr-only md:not-sr-only"
+              aria-live="polite"
+            >
+              {isMenuOpen ? 'Close' : 'Menu'}
             </span>
-          </span>
-        </button>
-      </header>
+            <span
+              className="grid items-center justify-center"
+              aria-hidden="true"
+            >
+              <MenuHex
+                className={classNames(
+                  'both-span-full',
+                  isMenuOpen && 'drop-shadow-xl'
+                )}
+              />
+              <span className="both-span-full flex justify-center text-orange scale-75">
+                <Burger
+                  color="currentColor"
+                  distance="sm"
+                  toggled={isMenuOpen}
+                  tabIndex={-1}
+                />
+              </span>
+            </span>
+          </button>
+        </header>
 
-      <section
-        className={classNames(
-          'fixed inset-0 z-40 h-screen flex bg-gray-dark transition-opacity',
-          !isMenuOpen && 'opacity-0 pointer-events-none'
-        )}
-      >
-        <img
-          src={menuBg}
-          className="absolute top-0 right-0"
-          aria-hidden="true"
-        />
+        <section
+          className={classNames(
+            'fixed inset-0 z-40 h-screen flex bg-gray-dark transition-opacity',
+            !isMenuOpen && 'opacity-0 pointer-events-none'
+          )}
+          aria-hidden={isMenuOpen ? 'false' : 'true'}
+        >
+          <img
+            src={menuBg}
+            className="absolute top-0 right-0"
+            aria-hidden="true"
+          />
+          <div
+            className={classNames(
+              'z-10 container flex flex-col',
+              !isMenuOpen && 'invisible'
+            )}
+          >
+            <div className="flex-grow flex flex-col lg:flex-row justify-center gap-16 lg:gap-4">
+              <nav className="lg:flex-grow lg:ml-[20%] flex flex-col justify-center items-center md:items-start gap-6 md:gap-0">
+                {data.primaryNav.links.map((link, index) => (
+                  <Link
+                    key={link.url}
+                    to={link.url}
+                    className="relative flex items-center gap-5"
+                  >
+                    <span
+                      className="absolute md:static top-1.5 -left-1.5 md:text-stroke-white text-xs md:text-4xl font-light md:font-black"
+                      aria-hidden="true"
+                    >
+                      0{index + 1}
+                    </span>
+                    <span
+                      className={classNames(
+                        'px-3 pb-2 md:pb-4 text-5xl md:text-8xl font-black md:font-medium leading-none hover:text-stroke-orange',
+                        link.url === currentPath && 'corners'
+                      )}
+                    >
+                      {link.label}
+                    </span>
+                  </Link>
+                ))}
+              </nav>
 
-        <div className="z-10 container flex flex-col">
-          <div className="flex-grow flex flex-col lg:flex-row justify-center gap-16 lg:gap-4">
-            <nav className="lg:flex-grow lg:ml-[20%] flex flex-col justify-center items-center md:items-start gap-6 md:gap-0">
-              {data.primaryNav.links.map((link, index) => (
-                <Link
-                  key={link.url}
-                  to={link.url}
-                  className="relative flex items-center gap-5"
-                >
-                  <span
-                    className="absolute md:static top-1.5 -left-1.5 md:text-stroke-white text-xs md:text-4xl font-light md:font-black"
-                    aria-hidden="true"
-                  >
-                    0{index + 1}
-                  </span>
-                  <span
-                    className={classNames(
-                      'px-3 pb-2 md:pb-4 text-5xl md:text-8xl font-black md:font-medium leading-none hover:text-stroke-orange',
-                      link.url === currentPath && 'corners'
-                    )}
-                  >
-                    {link.label}
-                  </span>
-                </Link>
+              <div className="lg:self-center hidden md:block lg:order-first">
+                <p className="max-w-xs text-white/50">
+                  {data.joinDescription.joinDescription}
+                </p>
+                <WaitlistButton className="button button-outline button-lg min-w-[240px] mt-7">
+                  {data.joinButtonLabel}
+                </WaitlistButton>
+              </div>
+            </div>
+
+            <div className="self-center flex flex-wrap justify-between gap-x-16 lg:gap-x-32 text-white/40 pb-2">
+              {data.secondaryNavs.map((nav) => (
+                <Nav links={nav.links} className="gap-8" />
               ))}
-            </nav>
 
-            <div className="lg:self-center hidden md:block lg:order-first">
-              <p className="max-w-xs text-white/50">
-                {data.joinDescription.joinDescription}
-              </p>
-              <button
-                type="button"
-                className="button button-lg button-outline mt-7"
-              >
-                {data.joinButtonLabel}
-              </button>
+              <SocialNav links={data.socialNav.links} className="gap-8" />
             </div>
           </div>
-
-          <div className="self-center flex flex-wrap justify-between gap-x-16 lg:gap-x-32 text-white/40 pb-2">
-            {data.secondaryNavs.map((nav) => (
-              <Nav links={nav.links} className="gap-8" />
-            ))}
-
-            <SocialNav links={data.socialNav.links} className="gap-8" />
-          </div>
-        </div>
-      </section>
-    </>
+        </section>
+      </div>
+    </FocusOn>
   )
 }
