7fdeea38cf6ab5adcd8edb18cc6d8ffd65295c4f
Austen Morgan <austen@boundaryla.com>
2022-01-05 18:26:29 -0800

    Remove contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,7 +20,7 @@ export default function PartnersPage(props) {
 
       <main>
         <section className="relative z-0 pt-40 lg:pt-56 pb-10">
-          <div className="absolute top-0 inset-x-0 z-[-1] flex justify-center overflow-hidden translate-y-[-37%] md:translate-y-[-22%] lg:translate-y-[-29%]">
+          <div className="absolute top-0 inset-x-0 z-[-1] flex justify-center overflow-hidden translate-y-[-35%] lg:translate-y-[-50%]">
             <img
               src={heroBg}
               className="max-w-none scale-[65%] md:scale-75 lg:scale-100 lg:translate-x-[14%]"
@@ -28,50 +28,18 @@ export default function PartnersPage(props) {
             />
           </div>
 
-          <div className="container lg:max-w-6xl flex flex-col lg:flex-row lg:justify-between items-center lg:items-start space-y-7 lg:space-y-0 lg:space-x-11">
-            <header className="flex flex-col space-y-3 md:space-y-5 max-w-[280px] md:max-w-lg lg:max-w-sm text-center lg:text-left">
+          <div className="container">
+            <header className="flex flex-col space-y-3 md:space-y-7 max-w-sm lg:max-w-xl mx-auto text-center">
               <h1 className="text-heading2 font-black uppercase">
                 <MDXRenderer>{heroData.title.childMdx.body}</MDXRenderer>
               </h1>
-              <p className="text-heading4 text-teal">{heroData.subtitle}</p>
-              <p className="text-white/50 leading-snug">
+              {heroData.subtitle && (
+                <p className="text-heading4 text-teal">{heroData.subtitle}</p>
+              )}
+              <p className="text-lg text-white/50 leading-snug">
                 {heroData.description.description}
               </p>
             </header>
-
-            <form className="w-full flex flex-col space-y-2.5 max-w-xl lg:max-w-lg">
-              <label>
-                <span className="sr-only">Your Name</span>
-                <input
-                  type="text"
-                  placeholder="Your Name"
-                  className="form-input w-full"
-                />
-              </label>
-              <label>
-                <span className="sr-only">Your Email</span>
-                <input
-                  type="email"
-                  placeholder="Your Email"
-                  className="form-input w-full"
-                />
-              </label>
-              <label>
-                <span className="sr-only">Your Email</span>
-                <textarea
-                  placeholder="Your Message"
-                  className="form-input w-full"
-                />
-              </label>
-              <div className="mt-1 md:mt-2 text-center lg:text-right">
-                <button
-                  type="submit"
-                  className="button button-outline py-2 text-base"
-                >
-                  {heroData.buttonLabel}
-                </button>
-              </div>
-            </form>
           </div>
         </section>
 
@@ -126,11 +94,9 @@ export const query = graphql`
           body
         }
       }
-      subtitle
       description {
         description
       }
-      buttonLabel
     }
     contentfulContactMethods {
       contactMethods {
