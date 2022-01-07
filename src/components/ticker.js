4611577b1a8c82c946365a157f1974d595a2e6b1
Austen Morgan <austen@boundaryla.com>
2022-01-06 16:28:06 -0800

    Add reduced motion state for ticker

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -3,7 +3,7 @@ export default function Ticker(props) {
 
   return (
     <div className="py-1 overflow-hidden bg-orange font-bold text-sm lg:text-[17px] lg:leading-normal whitespace-nowrap">
-      <div className="flex space-x-24 animate-ticker md:animation-duration-20 xl:animation-duration-30">
+      <div className="flex space-x-24 animate-ticker md:animation-duration-20 xl:animation-duration-30 motion-reduce:!animate-none">
         <span>{children}</span>
         {Array(10).fill(<span aria-hidden="true">{children}</span>)}
       </div>
