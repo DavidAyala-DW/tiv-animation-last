bdb64bf98a90aed85fb4e8d8c850dc2737bfb42a
walshsp87 <walshsp87@gmail.com>
2022-01-11 14:48:54 -0500

    adjust ticker speed (#4)

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -5,7 +5,7 @@ export default function Ticker(props) {
 
   return (
     <div className="py-1 overflow-hidden bg-orange font-bold text-sm lg:text-[17px] lg:leading-normal whitespace-nowrap">
-      <div className="flex space-x-24 animate-ticker md:animation-duration-20 xl:animation-duration-30 motion-reduce:!animate-none">
+      <div className="flex space-x-24 animate-ticker animation-duration-20 sm:animation-duration-30 md:animation-duration-40 xl:animation-duration-50 2xl:animation-duration-50 motion-reduce:!animate-none">
         <span>{children}</span>
         {_times(10, (index) => (
           <span key={index} aria-hidden="true">
